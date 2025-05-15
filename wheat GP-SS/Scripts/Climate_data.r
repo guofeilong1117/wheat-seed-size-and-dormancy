635b2125b3490ea4bb753fba582a3d00a6805081
guofeilong1117 <guofeilong@ibcas.ac.cn>
2025-05-15 16:56:50 +0800

    Climate_data.r

---
+++ wheat GP-SS/Scripts/Climate_data.r
@@ -0,0 +1,41 @@
+###提取气候变量
+library(raster)
+setwd("F:/wheat/climate/wc2.1_2.5m_bio")
+temp1 <- raster("wc2.1_2.5m_bio_1.tif")
+temp12 <- raster("wc2.1_2.5m_bio_12.tif")
+location <- read.table("F:/wheat/climate/wheat-lot.txt", sep="\t",header=T, stringsAsFactors = F)
+head (location)
+lon <- location$lon
+lat <- location$lat
+samples <- data.frame(lon, lat)
+temp.data <- samples 
+temp.data$BIO1 <- extract(temp1, samples)
+temp.data$BIO12 <- extract(temp12, samples)
+head(temp.dat
+write.table(temp.data, "F:/wheat/climate/bioclim.txt", sep="\t", row.names = F,quote=F)
+
+#####依据环境变量聚类
+library(factoextra)
+library(dplyr)
+library(pacman)
+library(cluster)
+bc<-read.csv("F:/wheat/climate/wheat.csv",sep=',',header=TRUE)
+names(bc)
+bc.scaled<-scale(bc[2:3])
+d<-dist(bc.scaled)
+km <- eclust(bc[2:3], "kmeans", nstart = 25)#聚类的散点图
+fviz_gap_stat(km$gap_stat) 
+fviz_silhouette(km) # 轮廓图，每种聚类下面的分布情况
+set.seed(666)
+kmeans1<-kmeans(bc.scaled,centers=3,nstart = 25)
+fviz_cluster(object=kmeans1,data=bc[2:3],
+             ellipse.type = "euclid",star.plot=T,repel=T,
+             geom = ("point"),palette='jco',main="",
+             ggtheme=theme_minimal())+
+  theme(axis.title = element_blank())
+summary(kmeans1)
+kmeans1$cluster
+kmeans1$size
+aaa <- data.frame(bc[1:20], kmeans1$cluster)
+aaa<-arrange(aaa,kmeans1.cluster)
+write.table(aaa, "F:/wheat/climate/pop.xls", sep="\t", row.names = F,quote=F)
